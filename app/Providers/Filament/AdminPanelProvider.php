54dc34b7b1f914f5724afac451386bb20d722dcb
annajwasz <annajwaszahro@gmail.com>
2025-03-23 05:22:03 +0700

    register done

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
             // ->brandName('POLIJE')
             ->brandLogo(asset('images/logo-polije.png'))
             ->brandLogoHeight('3rem')
