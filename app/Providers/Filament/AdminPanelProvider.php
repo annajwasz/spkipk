b5e1e005be7cb1ccba59556a505d2d672c944834
annajwasz <annajwaszahro@gmail.com>
2025-04-30 12:23:16 +0700

    add policy

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -61,6 +61,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make(),
             ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             SubKriteriaSeeder::class,
             MahasiswaSeeder::class,
             SuperAdminPermissionSeeder::class,
+            PermissionSeeder::class,
         ]);
     }
 }
